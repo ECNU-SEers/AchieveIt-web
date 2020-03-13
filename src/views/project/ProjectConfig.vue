91611d7ead41c856410364f9d1b4bb6ae247f8d9
VVVVlu <1320822439@qq.com>
2020-03-13 19:05:50 +0800

    testing

--- src/views/project/ProjectConfig.vue
+++ src/views/project/ProjectConfig.vue
@@ -1,20 +1,20 @@
 <template>
-    <div>
-        <p v-permission="{name:'查看系统消息',type:'disabled'}">Hello word</p>
-        <p v-permission="'查看日志'">Hello word</p>
-        <button v-permission="['修改信息','修改密码']">编辑</button>
-    </div>
+  <div>
+    <p v-permission="{ name: '查看系统消息', type: 'disabled' }">
+      this is a config.
+    </p>
+    <p v-permission="'查看日志'">Hello word</p>
+    <button v-permission="['修改信息', '修改密码']">编辑</button>
+  </div>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {};
-    },
-    methods: {}
-  };
+export default {
+  data() {
+    return {};
+  },
+  methods: {}
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
