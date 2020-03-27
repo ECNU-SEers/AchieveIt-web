8491a2818728d37f569e31eb7b6770fd615f78b3
Morgan279 <appouksd.com@qq.com>
2020-03-28 00:16:21 +0800

    fix:Search

--- src/views/permission/role/index.vue
+++ src/views/permission/role/index.vue
@@ -1,85 +1,103 @@
 <template>
-    <div>
-        <AddRoleDialog :visibility.sync="showAddRoleDialog"/>
-        <EditRoleDialog :visibility.sync="showEditRoleDialog" v-bind="editingRoleInfo"/>
-        <PageHeader title="角色设置">
-            <Search/>
-            <el-button @click="onAddRole" type="primary" class="add-btn">新增</el-button>
-        </PageHeader>
-        <Pagination :current-page.sync="currentPage" :total="tableData.length">
-            <el-table :data="tableData" style="width:100%;">
-                <el-table-column type="index" :index="index => index+1"/>
-                <el-table-column v-for="(item,index) in tableHeader"
-                                 :key="index" :prop="item.prop"
-                                 :label="item.label"
-                />
-                <el-table-column label="操作">
-                    <template slot-scope="scope">
-                        <el-button @click="onEditRole(scope.row)" type="primary" plain size="mini">
-                            编辑
-                        </el-button>
-                        <el-button @click="onDeleteRole(scope.row)" type="danger" size="mini" plain>
-                            删除
-                        </el-button>
-                    </template>
-                </el-table-column>
-            </el-table>
-        </Pagination>
-    </div>
+  <div>
+    <AddRoleDialog :visibility.sync="showAddRoleDialog" />
+    <EditRoleDialog
+      :visibility.sync="showEditRoleDialog"
+      v-bind="editingRoleInfo"
+    />
+    <PageHeader title="角色设置">
+      <Search :query-search="querySearch" />
+      <el-button @click="onAddRole" type="primary" class="add-btn"
+        >新增</el-button
+      >
+    </PageHeader>
+    <Pagination :current-page.sync="currentPage" :total="tableData.length">
+      <el-table :data="tableData" style="width:100%;">
+        <el-table-column type="index" :index="index => index + 1" />
+        <el-table-column
+          v-for="(item, index) in tableHeader"
+          :key="index"
+          :prop="item.prop"
+          :label="item.label"
+        />
+        <el-table-column label="操作">
+          <template slot-scope="scope">
+            <el-button
+              @click="onEditRole(scope.row)"
+              type="primary"
+              plain
+              size="mini"
+            >
+              编辑
+            </el-button>
+            <el-button
+              @click="onDeleteRole(scope.row)"
+              type="danger"
+              size="mini"
+              plain
+            >
+              删除
+            </el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </Pagination>
+  </div>
 </template>
 
 <script>
-  import Search from '@/components/common/Search';
-  import PageHeader from '@/components/common/PageHeader';
-  import Pagination from '@/components/common/Pagination';
-  import AddRoleDialog from "@/views/permission/role/AddRoleDialog";
-  import EditRoleDialog from "@/views/permission/role/EditRoleDialog";
-  import {roleListTableHeader} from "../const";
-  import {getRoleList} from "@/api/permisssion";
+import Search from '@/components/common/Search';
+import PageHeader from '@/components/common/PageHeader';
+import Pagination from '@/components/common/Pagination';
+import AddRoleDialog from '@/views/permission/role/AddRoleDialog';
+import EditRoleDialog from '@/views/permission/role/EditRoleDialog';
+import { roleListTableHeader } from '../const';
+import { getRoleList } from '@/api/permisssion';
 
-  export default {
-    components: {
-      Search,
-      PageHeader,
-      Pagination,
-      AddRoleDialog,
-      EditRoleDialog
+export default {
+  components: {
+    Search,
+    PageHeader,
+    Pagination,
+    AddRoleDialog,
+    EditRoleDialog
+  },
+  data() {
+    return {
+      tableData: [],
+      currentPage: 1,
+      showAddRoleDialog: false,
+      showEditRoleDialog: false,
+      tableHeader: Object.freeze(roleListTableHeader),
+      editingRoleInfo: null
+    };
+  },
+  methods: {
+    onAddRole() {
+      this.showAddRoleDialog = true;
     },
-    data() {
-      return {
-        tableData: [],
-        currentPage: 1,
-        showAddRoleDialog: false,
-        showEditRoleDialog: false,
-        tableHeader: Object.freeze(roleListTableHeader),
-        editingRoleInfo: null
-      };
+    onEditRole(row) {
+      this.editingRoleInfo = row;
+      this.showEditRoleDialog = true;
     },
-    methods: {
-      onAddRole() {
-        this.showAddRoleDialog = true;
-      },
-      onEditRole(row) {
-        this.editingRoleInfo = row;
-        this.showEditRoleDialog = true;
-      },
-      onDeleteRole(row) {
-
-      },
-      getRoleListFromServe() {
-        getRoleList().then(res => this.tableData = res);
-      }
+    onDeleteRole(row) {},
+    getRoleListFromServe() {
+      getRoleList().then(res => (this.tableData = res));
     },
-    created() {
-      this.getRoleListFromServe();
+    querySearch(queryString, cb) {
+      cb([{ value: '111' }]);
     }
-  };
+  },
+  created() {
+    this.getRoleListFromServe();
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-    .add-btn {
-        margin-left: 20px;
-        border-radius: 3px;
-        width: 80px;
-    }
+.add-btn {
+  margin-left: 20px;
+  border-radius: 3px;
+  width: 80px;
+}
 </style>
