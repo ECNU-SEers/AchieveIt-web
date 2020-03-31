f9957fa881650d1efc84ff41bc3e1417a950ae39
Morgan279 <appouksd.com@qq.com>
2020-03-31 16:01:23 +0800

    merge all branches

--- src/views/worktime/const.js
+++ src/views/worktime/const.js
@@ -1,21 +1,30 @@
-import { dateFormat } from '../../util/date';
+import {dateFormat} from '../../util/date';
 
 export const workTimeCheckTableHeader = [
-  { label: '日期', prop: 'date' },
-  { label: '功能名称', prop: 'functionName' },
-  { label: '活动名称', prop: 'activityName' },
-  { label: '开始时间', prop: 'startTime' },
-  { label: '结束时间', prop: 'endTime' },
-  { label: '提交人', prop: 'submitterName' },
-  { label: '提交日期', prop: 'submissionDate' },
-  { label: '参与项目', prop: 'projectName' },
-  { label: '状态', prop: 'state' }
+  {label: '日期', prop: 'date', formatter: (r, c, v) => dateFormat(v)},
+  {label: '功能名称', prop: 'functionName'},
+  {label: '活动名称', prop: 'activityName'},
+  {
+    label: '开始时间', prop: 'startTime',
+    formatter: (r, c, v) => dateFormat(v, 'hh:mm')
+  },
+  {
+    label: '结束时间', prop: 'endTime',
+    formatter: (r, c, v) => dateFormat(v, 'hh:mm')
+  },
+  {label: '提交人', prop: 'submitterName'},
+  {
+    label: '提交日期', prop: 'submissionDate',
+    formatter: (r, c, v) => dateFormat(v)
+  },
+  {label: '参与项目', prop: 'projectName'},
+  {label: '状态', prop: 'state'}
 ];
 
 export const workTimeSubmitTableHeader = [
-  { label: '日期', prop: 'date', formatter: (r, c, v) => dateFormat(v) },
-  { label: '功能名称', prop: 'functionName' },
-  { label: '活动名称', prop: 'activityName' },
+  {label: '日期', prop: 'date', formatter: (r, c, v) => dateFormat(v)},
+  {label: '功能名称', prop: 'functionName'},
+  {label: '活动名称', prop: 'activityName'},
   {
     label: '开始时间',
     prop: 'startTime',
@@ -31,5 +40,5 @@ export const workTimeSubmitTableHeader = [
     prop: 'submissionDate',
     formatter: (r, c, v) => dateFormat(v)
   },
-  { label: '状态', prop: 'state' }
+  {label: '状态', prop: 'state'}
 ];
