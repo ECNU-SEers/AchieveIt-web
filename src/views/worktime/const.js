c120e3c493bf305e5b897f04d1df2b1b223dc019
Morgan279 <appouksd.com@qq.com>
2020-03-31 15:57:45 +0800

    merge all branches

--- src/views/worktime/const.js
+++ src/views/worktime/const.js
@@ -1,3 +1,5 @@
+import { dateFormat } from '../../util/date';
+
 export const workTimeCheckTableHeader = [
   { label: '日期', prop: 'date' },
   { label: '功能名称', prop: 'functionName' },
@@ -11,11 +13,23 @@ export const workTimeCheckTableHeader = [
 ];
 
 export const workTimeSubmitTableHeader = [
-  { label: '日期', prop: 'date' },
+  { label: '日期', prop: 'date', formatter: (r, c, v) => dateFormat(v) },
   { label: '功能名称', prop: 'functionName' },
   { label: '活动名称', prop: 'activityName' },
-  { label: '开始时间', prop: 'startTime' },
-  { label: '结束时间', prop: 'endTime' },
-  { label: '提交日期', prop: 'submissionDate' },
+  {
+    label: '开始时间',
+    prop: 'startTime',
+    formatter: (r, c, v) => dateFormat(v, 'hh:mm')
+  },
+  {
+    label: '结束时间',
+    prop: 'endTime',
+    formatter: (r, c, v) => dateFormat(v, 'hh:mm')
+  },
+  {
+    label: '提交日期',
+    prop: 'submissionDate',
+    formatter: (r, c, v) => dateFormat(v)
+  },
   { label: '状态', prop: 'state' }
 ];
