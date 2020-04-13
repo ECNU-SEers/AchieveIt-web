35a812c0f01bb30ccd04083b384f89bda0ab5cbf
Liang <coder_wclong@163.com>
2020-04-13 19:02:24 +0800

    fix: worktime check

--- src/api/workTime.js
+++ src/api/workTime.js
@@ -1,42 +1,42 @@
-import { get, post, put } from '@/sys/plugins/axios';
+import {get, post, put} from '@/sys/plugins/axios';
 
 const NODULE_NAME = '/laborhour';
 
 export const getWorkTimeCheckList = (pageNo, pageSize = 10) =>
-  get(`${NODULE_NAME}/show/subordinate/list`, { pageNo, pageSize });
+    get(`${NODULE_NAME}/show/subordinate/list`, {pageNo, pageSize});
 
-export const acceptWorkTime = row => put(`${NODULE_NAME}/accept`, row);
+export const acceptWorkTime = id => put(`${NODULE_NAME}/accept`, {id});
 
-export const refuseWorkTime = row => put(`${NODULE_NAME}/return`, row);
+export const refuseWorkTime = id => put(`${NODULE_NAME}/return`, {id});
 
 export const searchWorkTimeCheckList = (
-  startDateTimestamp,
-  endDateTimestamp,
-  pageNo,
-  pageSize = 10
-) =>
-  get(`${NODULE_NAME}/search/subordinate`, {
     startDateTimestamp,
     endDateTimestamp,
     pageNo,
-    pageSize
-  });
+    pageSize = 10
+) =>
+    get(`${NODULE_NAME}/search/subordinate`, {
+      startDateTimestamp,
+      endDateTimestamp,
+      pageNo,
+      pageSize
+    });
 
 export const getWorkTimeSubmitList = (pageNo, pageSize = 10) =>
-  get(`${NODULE_NAME}/show/list`, { pageNo, pageSize });
+    get(`${NODULE_NAME}/show/list`, {pageNo, pageSize});
 
 export const searchWorkTimeSubmitList = (
-  startDateTimestamp,
-  endDateTimestamp,
-  pageNo,
-  pageSize = 10
-) =>
-  get(`${NODULE_NAME}/search`, {
     startDateTimestamp,
     endDateTimestamp,
     pageNo,
-    pageSize
-  });
+    pageSize = 10
+) =>
+    get(`${NODULE_NAME}/search`, {
+      startDateTimestamp,
+      endDateTimestamp,
+      pageNo,
+      pageSize
+    });
 
 export const submitWorkTime = row => post(`${NODULE_NAME}/create`, row);
 
--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -1,144 +1,144 @@
 <template>
-  <div class="container">
-    <p class="title">AchieveIt 软件项目管理平台</p>
-    <el-card class="login-box bg-translucence" shadow="hover">
-      <el-form
-        ref="form"
-        label-position="left"
-        :model="form"
-        :rules="rules"
-        label-width="60"
-      >
-        <el-form-item label="账号:" prop="username">
-          <el-input v-model="form.username" />
-        </el-form-item>
-        <el-form-item label="密码:" prop="password">
-          <el-input v-model="form.password" type="password" />
-        </el-form-item>
-      </el-form>
-      <el-button
-        type="primary"
-        class="login-btn btn"
-        @click="userLogin"
-        :loading="loading"
-        :disabled="loading"
-      >
-        登录
-      </el-button>
-    </el-card>
-  </div>
+    <div class="container">
+        <p class="title">AchieveIt 软件项目管理平台</p>
+        <el-card class="login-box bg-translucence" shadow="hover">
+            <el-form
+                    ref="form"
+                    label-position="left"
+                    :model="form"
+                    :rules="rules"
+                    label-width="60"
+            >
+                <el-form-item label="账号:" prop="username">
+                    <el-input v-model="form.username"/>
+                </el-form-item>
+                <el-form-item label="密码:" prop="password">
+                    <el-input v-model="form.password" type="password"/>
+                </el-form-item>
+            </el-form>
+            <el-button
+                    type="primary"
+                    class="login-btn btn"
+                    @click="userLogin"
+                    :loading="loading"
+                    :disabled="loading"
+            >
+                登录
+            </el-button>
+        </el-card>
+    </div>
 </template>
 
 <script>
-import { mapActions, mapMutations } from "vuex";
-import User from "@/sys/models/user";
-import Utils from "@/sys/utils/util";
+  import {mapActions, mapMutations} from "vuex";
+  import User from "@/sys/models/user";
+  import Utils from "@/sys/utils/util";
 
-export default {
-  data() {
-    return {
-      loading: false, // 加载动画
-      wait: 2000, // 2000ms之内不能重复发起请求
-      throttleLogin: null, // 节流登录
-      form: {
-        username: "admin",
-        password: "admin"
+  export default {
+    data() {
+      return {
+        loading: false, // 加载动画
+        wait: 2000, // 2000ms之内不能重复发起请求
+        throttleLogin: null, // 节流登录
+        form: {
+          username: "",
+          password: ""
+        },
+        rules: {
+          username: [
+            {
+              required: true,
+              message: "请输入账号"
+            }
+          ],
+          password: [
+            {
+              required: true,
+              message: "请输入密码"
+            },
+            {
+              min: 6,
+              message: "长度至少为6位"
+            }
+          ]
+        }
+      };
+    },
+    methods: {
+      async userLogin() {
+        const {username, password} = this.form;
+        await this.login(username, password);
       },
-      rules: {
-        username: [
-          {
-            required: true,
-            message: "请输入账号"
-          }
-        ],
-        password: [
-          {
-            required: true,
-            message: "请输入密码"
-          },
-          {
-            min: 6,
-            message: "长度至少为6位"
-          }
-        ]
-      }
-    };
-  },
-  methods: {
-    async userLogin() {
-      const { username, password } = this.form;
-      await this.login(username, password);
+      async login(username, password) {
+        try {
+          this.loading = true;
+          await User.getToken(username, password);
+          await this.getInformation();
+          this.loading = false;
+          this.$router.push("/about");
+          this.$message.success("登录成功");
+        } catch (e) {
+          this.loading = false;
+          console.log(e);
+        }
+      },
+      async getInformation() {
+        try {
+          // 尝试获取当前用户信息
+          const user = await User.getPermissions();
+          this.setUserAndState(user);
+          this.setUserPermissions(user.permissions);
+        } catch (e) {
+          console.log(e);
+        }
+      },
+      ...mapActions(["setUserAndState"]),
+      ...mapMutations({
+        setUserPermissions: "SET_USER_PERMISSIONS"
+      })
     },
-    async login(username, password) {
-      try {
-        this.loading = true;
-        await User.getToken(username, password);
-        await this.getInformation();
-        this.loading = false;
-        this.$router.push("/about");
-        this.$message.success("登录成功");
-      } catch (e) {
-        this.loading = false;
-        console.log(e);
-      }
-    },
-    async getInformation() {
-      try {
-        // 尝试获取当前用户信息
-        const user = await User.getPermissions();
-        this.setUserAndState(user);
-        this.setUserPermissions(user.permissions);
-      } catch (e) {
-        console.log(e);
-      }
-    },
-    ...mapActions(["setUserAndState"]),
-    ...mapMutations({
-      setUserPermissions: "SET_USER_PERMISSIONS"
-    })
-  },
-  created() {
-    // 节流登录
-    this.throttleLogin = Utils.throttle(this.login, this.wait);
-  }
-};
+    created() {
+      // 节流登录
+      this.throttleLogin = Utils.throttle(this.login, this.wait);
+    }
+  };
 </script>
 
 <style lang="scss" scoped>
-.container {
-  width: 100%;
-  height: 100%;
-  background-image: url("../../assets/img/login-bg.jpg");
-  background-size: cover;
-  position: fixed;
-}
+    .container {
+        width: 100%;
+        height: 100%;
+        background-image: url("../../assets/img/login-bg.jpg");
+        background-size: cover;
+        position: fixed;
+    }
 
-.title {
-  font-size: 48px;
-  margin-top: 160px;
-}
+    .title {
+        font-size: 48px;
+        margin-top: 160px;
+    }
 
-.login-box {
-  width: 480px;
-  height: 320px;
-  margin: 80px auto;
-  text-align: center;
-}
+    .login-box {
+        width: 480px;
+        height: 320px;
+        margin: 80px auto;
+        text-align: center;
+    }
 
-.bg-translucence {
-  background-color: rgba(255, 255, 255, 0.4);
-}
+    .bg-translucence {
+        background-color: rgba(255, 255, 255, 0.4);
+    }
 
-.btn {
-  width: 440px;
-  height: 40px;
-}
+    .btn {
+        width: 440px;
+        height: 40px;
+    }
 
-.login-btn {
-  margin-bottom: 16px;
-}
+    .login-btn {
+        margin-bottom: 16px;
+    }
 
-.tourist-btn {
-  margin-left: -2px;
-}
+    .tourist-btn {
+        margin-left: -2px;
+    }
 </style>
