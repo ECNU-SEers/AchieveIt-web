dd8a3d9d15545b6e4b2c35690fe4975c5cd57468
Liang <coder_wclong@163.com>
2020-04-13 22:03:13 +0800

    fix: allow empty role name

--- src/views/permission/role/EditRoleDialog.vue
+++ src/views/permission/role/EditRoleDialog.vue
@@ -1,75 +1,80 @@
 <template>
-  <el-dialog title="编辑角色" :visible="visibility" :before-close="close">
-    <el-form v-model="form">
-      <el-form-item label="角色名称">
-        <el-input v-model="form.name" />
-      </el-form-item>
-      <el-form-item label="创建人">
-        <el-input readonly disabled :value="creator" />
-      </el-form-item>
-      <el-form-item>
-        <RolePermissions
-          @updatePermissions="updatePermissions"
-          @updateAllPermissions="updateAllPermissions"
-          title="分配权限"
-          :permissions="permissions"
-        >
-        </RolePermissions>
-      </el-form-item>
-    </el-form>
-    <span slot="footer">
+    <el-dialog title="编辑角色" :visible="visibility" :before-close="close">
+        <el-form :model="form" :rules="rules">
+            <el-form-item prop="name" label="角色名称">
+                <el-input v-model="form.name"/>
+            </el-form-item>
+            <el-form-item label="创建人">
+                <el-input readonly disabled :value="creator"/>
+            </el-form-item>
+            <el-form-item>
+                <RolePermissions
+                        @updatePermissions="updatePermissions"
+                        @updateAllPermissions="updateAllPermissions"
+                        title="分配权限"
+                        :permissions="permissions"
+                >
+                </RolePermissions>
+            </el-form-item>
+        </el-form>
+        <span slot="footer">
       <el-button @click="close">取 消</el-button>
       <el-button ref="btn" type="primary" @click="onEditRole">确 定</el-button>
     </span>
-  </el-dialog>
+    </el-dialog>
 </template>
 
 <script>
-import RolePermissions from '@/views/admin/role/RolePermissions';
-import { editUserRole } from '@/api/permisssion';
-import { permissions } from '../../../store/getters';
-import { dialogMixin } from '../../../util/mixin';
+  import RolePermissions from '@/views/admin/role/RolePermissions';
+  import {editUserRole} from '@/api/permisssion';
+  import {permissions} from '../../../store/getters';
+  import {dialogMixin} from '../../../util/mixin';
+  import {isEmpty} from "lodash";
 
-export default {
-  components: {
-    RolePermissions
-  },
-  props: {
-    id: Number,
-    name: String,
-    creator: String,
-    permissions: {
-      type: Array,
-      default: () => []
-    }
-  },
-  mixins: [dialogMixin],
-  data() {
-    return {
-      allPermissions: [], // 所有权限
-      form: {
-        name: this.name,
-        permissions: this.permissions
+  export default {
+    components: {
+      RolePermissions
+    },
+    props: {
+      id: Number,
+      name: String,
+      creator: String,
+      permissions: {
+        type: Array,
+        default: () => []
       }
-    };
-  },
-  methods: {
-    updatePermissions(permissions) {
-      this.form.permissions = permissions;
     },
-    updateAllPermissions(allPermissions) {
-      this.allPermissions = allPermissions;
+    mixins: [dialogMixin],
+    data() {
+      return {
+        allPermissions: [], // 所有权限
+        form: {
+          name: this.name,
+          permissions: this.permissions
+        },
+        rules: [{
+          name: [{required: true, message: '请输入角色名'}]
+        }]
+      };
     },
-    onEditRole() {
-      const { name, permissions } = this.form;
-      editUserRole(this.id, name, permissions).then(() => {
-        this.$message.success('更新角色成功');
-        this.close();
-        this.$emit('success');
-      });
+    methods: {
+      updatePermissions(permissions) {
+        this.form.permissions = permissions;
+      },
+      updateAllPermissions(allPermissions) {
+        this.allPermissions = allPermissions;
+      },
+      onEditRole() {
+        const {name, permissions} = this.form;
+        if (isEmpty(name)) return this.$message.error('请输入角色名');
+        editUserRole(this.id, name, permissions).then(() => {
+          this.$message.success('更新角色成功');
+          this.close();
+          this.$emit('success');
+        });
+      }
     }
-  }
-};
+  };
 </script>
 
 <style lang="scss" scoped></style>
